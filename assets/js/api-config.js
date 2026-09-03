eeb7274202eec4fb4c4b12bbbfe8b0ab0075aaa0
Shahisha <shahisha123adh@gmail.com>
2026-09-03 22:47:23 +0545

    Remove pages and assets:

--- assets/js/api-config.js
+++
@@ -1,1 +0,0 @@
-export const API_BASE = "https://proglog-rawg-proxy.shahisha123adh.workers.dev";
--- assets/js/firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "proglog-fa459.firebaseapp.com",
-  projectId: "proglog-fa459",
-  storageBucket: "proglog-fa459.firebasestorage.app",
-  messagingSenderId: "",
-  appId: "",
-};
